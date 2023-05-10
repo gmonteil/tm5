4a7616963e8ce30f67a80157dc9b844a811e2ac0
Guillaume Monteil <gmonteil@ik.me>
2023-05-10 14:22:36 +0200

    Enabled running TM5 without singularity container

--- tm5/run.py
+++ tm5/run.py
@@ -6,14 +6,15 @@ from loguru import logger
 from pathlib import Path
 
 
-def run_tm5(cmd: Union[str, List[str], List[Path]], settings : Dict = None):
+def run_tm5(cmd: str | List[str] | List[Path], settings : Dict = None):
     """
-    Run a command inside a singularity container (preconfigured for TM5).
+    Run a command, possibly inside a singularity container (preconfigured for TM5).
     Arguments :
         * cmd : the command to run (can be a string or a list of strings)
         * settings : should be a dictionary (or an omegaconf.DictConfig) with the following keys:
+            - container: path towards the singularity container (*.simg file). If the "container" key is not provided, then the command "cmd"
+             is just run as a subprocess.
             - tm5_path: points toward the path to be mounted under "/tm5"
-            - container: path towards the singularity container (*.simg file)
             - bind (optional): contains a list of additional paths of the host that should be mounted inside the container.
 
     Example :
@@ -23,20 +24,24 @@ def run_tm5(cmd: Union[str, List[str], List[Path]], settings : Dict = None):
                     'tm5_path': '/home/user/TM5',
                     'container': '/home/user/TM5/images/tm5.sif',
                     'bind': ['/data', '/scratch']})
+
+    By contrast, the command "run_tm5('ipython', settings={'tm5_path':'/home/user/TM5'})" will just spawn a ipython shell in a subprocess, since no "container" key is provided in "settings". The other keys in "settings" will be ignored.
     """
 
     # Ensure that the command is given as a list of strings
     if isinstance(cmd, str):
         cmd = cmd.split()
 
-    # Prepare the singularity-related part of the command
-    command = f'singularity run --cleanenv --bind {settings["tm5_path"]}:/tm5'
-    for mountpath in settings.get('bind', []):
-        command += f' --bind {mountpath}'
-    command += f' {settings["container"]} '
+    if 'container' in settings :
+        # Prepare the singularity-related part of the command
+        command = f'singularity run --cleanenv --bind {settings["tm5_path"]}:/tm5'
+        for mountpath in settings.get('bind', []):
+            command += f' --bind {mountpath}'
+        command += f' {settings["container"]} '
 
-    # Append the actual command, log and run:
-    cmd = command.split() + cmd
+        # Append the actual command, log and run:
+        cmd = command.split() + cmd
+
     logger.info(' '.join([str(_) for _ in cmd]))
     try :
         return subprocess.run(cmd)
