d97ca0626a5a65a2941e0d6413825a13c41747d2
Michael Vossbeck <Michael.Vossbeck@Inversion-Lab.com>
2025-03-14 14:36:24 +0100

    fix: -> coarsen_file (emissions)    - reverted to the previous re-gridding implementation      (the xesmf based approach is potentially still available by a switch)

--- tm5/emissions.py
+++ tm5/emissions.py
@@ -13,21 +13,77 @@ from loguru import logger
 from tqdm.auto import tqdm
 
 
-def coarsen_file(path_or_pattern : str, reg : TM5Grids, start : Timestamp, end : Timestamp) -> xr.Dataset:
+def coarsen_file(path_or_pattern : str, reg : TM5Grids, start : Timestamp, end : Timestamp, use_esmf_regridder : bool = False) -> xr.Dataset:
     # Open the source file(s). It should be on a global 1x1 resolution (for now ...)
     ds = xr.open_mfdataset(path_or_pattern)
 
-    # Define a regridder
+    #-- expected grid of global emission inputs
+    # !!!TODO:should add consistency check
     glb1x1 = TM5Grids.global1x1()
-    regridder = glb1x1.regridder(reg)
 
-    # Regrid and ensure the result is on a daily time step (for now ...)
-    coarse = regridder(ds)
+    #
+    #-- MVO 2025-03-14::
+    #   - re-gridding results seem not appropriate
+    #     with ESMF regridder used underneath,
+    #     by default apply again approach as used in old implementation
+    #
+    # Regrid
+    if use_esmf_regridder:
+        # Define a regridder
+        regridder = glb1x1.regridder(reg)
+        coarse = regridder(ds)
+    else:
+        #
+        #-- iterate over (emission) datasets and
+        #   (1) crop to target region
+        #   (2) re-grid to target resolution
+        data_coarse_dict = {}
+        for da_name in ds.data_vars:
+            if da_name=='area':
+                continue
+            da = ds[da_name]
+            #-- make sure units is as expected
+            if not da.units=='kg/cl/s':
+                msg = f"emission {da_name} with unexpected units ==>{da.units}<=="
+                raise RuntimeError(msg)
+            #-- (1) crop (result is numpy array)
+            data_out = da.sel(lat=slice(reg.south, reg.north),
+                              lon=slice(reg.west, reg.east)).values
+            #-- (2) coarsen (only if needed):
+            if (reg.dlat!=glb1x1.dlat) or (reg.dlon!=glb1x1.dlon):
+                assert (reg.dlat - int(reg.dlat) == 0) & (reg.dlon - int(reg.dlon) == 0), "lat and lon steps must be multiple of the base (1°) step"
+                nt, nlat, nlon = data_out.shape
+                data_out = data_out.reshape(nt, -1, int(reg.dlat), nlon).sum(2)
+                nlat = data_out.shape[1]
+                data_out = data_out.reshape(nt, nlat, -1, int(reg.dlon)).sum(3)
+            data_coarse_dict[da_name] = xr.DataArray(
+                data_out,
+                dims = ('time', 'lat', 'lon'),
+                coords = {
+                    'time': ds.time,
+                    'lat': reg.latc,
+                    'lon': reg.lonc
+                }
+            )
+        #
+        #-- create xarray dataset
+        #
+        coarse = xr.Dataset(
+            coords=dict(lon=('lon', reg.lonc),
+                        lat=('lat', reg.latc),
+                        time=ds['time']),
+            data_vars=data_coarse_dict
+            )
+
+    #
+    # ensure result is on a daily time step (at least for  now ...)
+    #
     coarse = coarse.reindex(time=date_range(start, end, freq='D')).ffill('time')
-
-    # TM5 expects float64 precision 
+    #
+    # ensure float64 precision (as TM5 I/O expects)
+    #
     coarse = coarse.astype('f8')
-    # Return ordered the way TM5 wants it!
+
     return coarse
 
 
@@ -53,7 +109,6 @@ def prepare_emissions(conf: DictConfig) -> None:
             # Do the actual coarsening.
             for catname, cat in tracer.categories.items():
                 datasets[catname] = coarsen_file(cat.path, reg, start, end)[cat.field]
-
             # Ensure that the dest path exists!
             Path(tracer.prefix).parent.mkdir(exist_ok=True, parents=True)
 
