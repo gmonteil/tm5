2a1a9f635340df8903820892c7b7f65d2e5a77f7
Michael Vossbeck <Michael.Vossbeck@Inversion-Lab.com>
2025-07-30 10:36:21 +0200

    modified: make init not fail in case .config/fitic/gui.conf is missing

--- tm5/gui/__init__.py
+++ tm5/gui/__init__.py
@@ -1,3 +1,6 @@
 from omegaconf import OmegaConf
 from pathlib import Path
-host = OmegaConf.load(Path.home() / ".config/fitic/gui.conf")
+try:
+    host = OmegaConf.load(Path.home() / ".config/fitic/gui.conf")
+except FileNotFoundError:
+    host = None
