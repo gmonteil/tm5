aeb54bc03762d173b2062b1f1cc173258cef0c1a
Michael Vossbeck <Michael.Vossbeck@Inversion-Lab.com>
2026-01-19 15:50:43 +0100

    modified: -> write_adj_emis    - write file to dedicated destination directory (rather than the      current working directory)

--- src/emissions/emission_adj.F90
+++ src/emissions/emission_adj.F90
@@ -119,65 +119,81 @@ module Emission_Adj
 
         subroutine write_adj_emis(ireg, tr, status)
 
-            use file_netcdf,    only : nc_open, nc_close, nc_create_dim, nc_dump_var
-            use chem_param,     only : names
-            use dims,           only : itaue, itaui, itau
-            use datetime,       only : tau2date
+          use file_netcdf,    only : nc_open, nc_close, nc_create_dim, nc_dump_var
+          use global_data,    only : RcF, outdir
+          use go,             only : pathsep, ReadRc
+          use os_specs,       only : MAX_FILENAME_LEN, MAX_RCKEY_LEN
+          use chem_param,     only : names
+          use dims,           only : itaue, itaui, itau
+          use datetime,       only : tau2date
 
-            ! This is called by "emission_adj_apply" at the end of each timestep (?)
-            !
-            ! In the forward we read a new emis if the *current* filename is different from the *previous* filename
-            ! Here we need to write a new adj_emis if the *current* filename is different than the *next* filename (otherwise, we accumulate)
+          ! This is called by "emission_adj_apply" at the end of each timestep (?)
+          !
+          ! In the forward we read a new emis if the *current* filename is different from the *previous* filename
+          ! Here we need to write a new adj_emis if the *current* filename is different than the *next* filename (otherwise, we accumulate)
 
-            integer, intent(in)                     :: ireg
-            integer, intent(out)                    :: status
-            type(tdate), dimension(2), intent(in)   :: tr
+          integer, intent(in)                     :: ireg
+          integer, intent(out)                    :: status
+          type(tdate), dimension(2), intent(in)   :: tr
 
-            character(len=*), parameter             :: rname = mname//'/write_adj_apply'
-            character(len=MAX_FILENAME_LEN)         :: filename
+          character(len=*), parameter             :: rname = mname//'/write_adj_emis'
+          character(len=MAX_FILENAME_LEN)         :: filename
+          character(len=MAX_RCKEY_LEN)            :: subdir_adjemis
+          logical                                 :: dir_exist
+          integer                                 :: fid
+          real    :: nsec
 
-            integer                                 :: fid
-            real    :: nsec
+          status = 0
 
-            status = 0
+          ! Unless the current step is exactly at midnight, just keep accumulating adjoint fields
+          if ((tr(2)%hour + tr(2)%min + tr(2)%sec) > 0) return
 
-            ! Unless the current step is exactly at midnight, just keep accumulating adjoint fields
-            if ((tr(2)%hour + tr(2)%min + tr(2)%sec) > 0) return
+          ! Calculate the number of sec since the start of the simulation
+          nsec = itaue - itau - abs(rtotal(tr(2) - tr(1), 'sec'))
 
-            ! Calculate the number of sec since the start of the simulation
-            nsec = itaue - itau - abs(rtotal(tr(2) - tr(1), 'sec'))
+          call ReadRc(rcF, 'output.adjemis', &
+               subdir_adjemis, status, default='adjemis')
+          IF_ERROR_RETURN(status=1)
+          !-- make sure directory exists
+          inquire(file=trim(outdir)//pathsep//trim(subdir_adjemis), exist=dir_exist)
+          if( .not. dir_exist ) &
+               call system('mkdir -p '//trim(outdir)//pathsep//trim(subdir_adjemis))
+          ! Write the adjoint emissions to a netCDF file (TODO: write the code!)
+          write(filename, '(4a,"adjemis.", a, ".", i4.4, 2i2.2, ".nc")'), &
+               trim(outdir), pathsep, trim(subdir_adjemis), pathsep,&
+               trim(region_name(ireg)), tr(2)%year, tr(2)%month, tr(2)%day
+          
+          fid = nc_open(trim(filename), 'c', status)
+          IF_NOTOK_RETURN(status=1)
 
-            ! Write the adjoint emissions to a netCDF file (TODO: write the code!)
-            write(filename, '("adjemis.", a, ".", i4.4, 2i2.2, ".nc")'), trim(region_name(ireg)), tr(2)%year, tr(2)%month, tr(2)%day
-            fid = nc_open(trim(filename), 'c', status)
-            IF_NOTOK_RETURN(status=1)
+          ! Create dimensions
+          call nc_create_dim(fid, "lat", lli(ireg)%nlat)
+          !call nc_create_dim(fid, "lat", jer(ireg) - jsr(ireg) + 1)
+          call nc_create_dim(fid, 'lon', lli(ireg)%nlon)
+          !call nc_create_dim(fid, 'lon', ier(ireg) - isr(ireg) + 1)
+          call nc_create_dim(fid, 'tracer', ntracet)
+          call nc_create_dim(fid, 'time', 1)
 
-            ! Create dimensions
-            call nc_create_dim(fid, "lat", lli(ireg)%nlat)
-            !call nc_create_dim(fid, "lat", jer(ireg) - jsr(ireg) + 1)
-            call nc_create_dim(fid, 'lon', lli(ireg)%nlon)
-            !call nc_create_dim(fid, 'lon', ier(ireg) - isr(ireg) + 1)
-            call nc_create_dim(fid, 'tracer', ntracet)
-            call nc_create_dim(fid, 'time', 1)
+          ! Create coordinates
+          ! print*, ireg, lli(ireg)%lat_deg
+          call nc_dump_var(fid, 'lat', ['lat'], lli(ireg)%lat_deg, ['units    '], ['degrees_north'])
+          !call nc_dump_var(fid, 'lat', ['latitude'], lli(ireg)%lat_deg(jsr(ireg):jer(ireg)), ['units    '], ['degrees_north'])
+          call nc_dump_var(fid, 'lon', ['lon'], lli(ireg)%lon_deg, ['units    '], ['degrees_east'])
+          !call nc_dump_var(fid, 'lon', ['longitude'], lli(ireg)%lon_deg(isr(ireg):ier(ireg)), ['units    '], ['degrees_east'])
+          call nc_dump_var(fid, 'time', ['time'], [nsec])
 
-            ! Create coordinates
-            print*, ireg, lli(ireg)%lat_deg
-            call nc_dump_var(fid, 'lat', ['lat'], lli(ireg)%lat_deg, ['units    '], ['degrees_north'])
-            !call nc_dump_var(fid, 'lat', ['latitude'], lli(ireg)%lat_deg(jsr(ireg):jer(ireg)), ['units    '], ['degrees_north'])
-            call nc_dump_var(fid, 'lon', ['lon'], lli(ireg)%lon_deg, ['units    '], ['degrees_east'])
-            !call nc_dump_var(fid, 'lon', ['longitude'], lli(ireg)%lon_deg(isr(ireg):ier(ireg)), ['units    '], ['degrees_east'])
-            call nc_dump_var(fid, 'time', ['time'], [nsec])
+          ! Create and the adjoint emission field
+          !call nc_dump_var(fid, 'adj_emis', ['tracer   ', 'latitude ', 'longitude'], adj_emis(ireg)%values)
+          call nc_dump_var(fid, 'adj_emis', ['lon', 'lat ', 'tracer   '], adj_emis(ireg)%values)
+          ! print*, shape(adj_emis(ireg)%values)
+          ! print*, lli(ireg)%nlon, lli(ireg)%nlat
 
-            ! Create and the adjoint emission field
-            !call nc_dump_var(fid, 'adj_emis', ['tracer   ', 'latitude ', 'longitude'], adj_emis(ireg)%values)
-            call nc_dump_var(fid, 'adj_emis', ['lon', 'lat ', 'tracer   '], adj_emis(ireg)%values)
-            print*, shape(adj_emis(ireg)%values)
-            print*, lli(ireg)%nlon, lli(ireg)%nlat
+          call nc_close(fid)
 
-            call nc_close(fid)
-
-            ! Reset the adjoint fields
-            adj_emis(ireg)%values = 0
+          !-- MVO-COMMENT:maybe better to move this into the calling context?
+          !               (to not have side-effect in this I/O routine)
+          ! Reset the adjoint fields
+          adj_emis(ireg)%values = 0
 
         end subroutine write_adj_emis
 
