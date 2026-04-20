42aec14244bb34e2e53dafd9d77c4ccd03bb5799
Michael Vossbeck <Michael.Vossbeck@Inversion-Lab.com>
2026-04-20 12:34:35 +0200

    modified: -> bit more flexibility for emissions preparation script

--- run/prepare_emis.py
+++ run/prepare_emis.py
@@ -1,56 +1,79 @@
 #!/usr/bin/env python
 
 # Prepare daily emission files for the requested grid
-
+import os
+import sys
+from omegaconf import OmegaConf
 import xarray as xr
-from tm5.gridtools import TM5Grids
 from pandas import Timestamp, date_range
 from tm5.settings import load_config
-import os
 from pathlib import Path
+from loguru import logger
+from argparse import ArgumentParser
+from tm5.gridtools import TM5Grids
+from tm5.emissions import prepare_emissions, coarsen_file
 
-def coarsen_file(path_or_pattern : str, reg : TM5Grids, start : Timestamp, end : Timestamp) -> xr.Dataset:
-    # Open the source file(s). It should be on a global 1x1 resolution (for now ...)
-    print(path_or_pattern)
-    ds = xr.open_mfdataset(path_or_pattern)
+try:
+    TM5_HOST = os.environ['TM5_HOST']
+except KeyError:
+    TM5_HOST = None
 
-    # Define a regridder
-    glb1x1 = TM5Grids.global1x1()
-    regridder = glb1x1.regridder(reg)
+parser = ArgumentParser()
+parser.add_argument('--config_file', default='forward.yaml')
+parser.add_argument('-m', '--host', default=TM5_HOST)
+parser.add_argument('--expdir',
+                    help="""override target dir from host section in yaml file.""")
+parser.add_argument('--outdir',
+                    help="""override output directory of selected machine in yaml file.""")
+parser.add_argument('--trange',
+                    metavar=('tstart','tend'),
+                    nargs=2,
+                    help="""whether to override simulation start/end time specified in the yaml file (strings must be parseable as pandas Timestamp).""")
+args = parser.parse_args(sys.argv[1:])
 
-    # Regrid and ensure the result is on a daily time step (for now ...)
-    coarse = regridder(ds)
-    coarse.reindex(time=date_range(start, end, freq='D')).ffill('time')
+if args.host==None:
+    msg = f"host must be specified, either via TM5_HOST environment " \
+        "variable or by using option --host. Cannot continue!"
+    raise RuntimeError(msg)
 
-    # Return ordered the way TM5 wants it!
-    return coarse.transpose('lon', 'lat', 'time')
+yaml_file = Path(args.config_file)
+if not yaml_file.exists():
+    msg = f"provided yaml file ***{str(yaml_file)}*** not accessible!"
+    raise RuntimeError(msg)
 
-# Load config file
-conf = load_config('forward.yaml', host=os.environ['TM5_HOST'])
+#=====================================================
+# 1. read configuration, potentiall override part of the settings, set 'host' key
+#=====================================================
+dconf = OmegaConf.load(str(yaml_file))
+#-- consistency
+if not args.host in dconf.keys():
+    msg = f"selected machine -->{args.host}<-- not found in configuration file."
+    raise RuntimeError(msg)
+#
+#-- override settings
+#
+if args.trange!=None:
+    tstart, tend = args.trange
+    dconf.run['start'] = tstart
+    dconf.run['end']   = tend
+if args.outdir!=None:
+    outdir_sav = dconf[args.host].paths.output
+    msg = f"setting experiment directory -->{args.outdir}<-- " \
+        f"(overriding configuration file ==>{outdir_sav}<=="
+    logger.info(msg)
+    dconf[args.host].paths.output = args.outdir
+dconf['host'] = dconf[args.host]
 
-# Read start and end
-start = Timestamp(conf.run.start)
-end = Timestamp(conf.run.end)
+#=====================================================
+# copy originating yaml file
+#=====================================================
+dst = Path(dconf.run.paths.output) / yaml_file.name
+dst.parent.mkdir(parents=True, exist_ok=True)
+OmegaConf.save(config=dconf, f=str(dst))
+msg = f"...stored yaml configuration file ***{str(dst)}***"
+logger.info(msg)
 
-# This should write one emission file per day, region and tracer, containing all the emissions of that tracer
-for region in conf.run.regions :
-
-    # Create the region object
-    reg = conf.regions[region]
-    reg = TM5Grids.from_corners(west=reg.lons[0], east=reg.lons[1], south=reg.lats[0], north=reg.lats[1], dlon=reg.lons[2], dlat=reg.lats[2])
-
-    # regrid the emissions for that region and tracer
-    for trname, tracer in conf.emissions.items():
-        datasets = {}
-
-        # Do the actual coarsening.
-        for catname, cat in tracer.categories.items():
-            datasets[catname] = coarsen_file(cat.path, reg, start, end)[cat.field]
-
-        # Ensure that the dest path exists!
-        Path(tracer.prefix).parent.mkdir(exist_ok=True, parents=True)
-
-        # group the datasets in daily emission files for that region and tracer:
-        for day in date_range(start, end, freq='D'):
-            destfile = f'{tracer.prefix}.{trname}.{region}.{day:%Y%m%d}.nc'
-            xr.Dataset({k:datasets[k].sel(time=day).transpose() for k in datasets}).to_netcdf(destfile)
+#=====================================================
+# 2. prepare (daily) emissions files 
+#=====================================================
+prepare_emissions(dconf)
