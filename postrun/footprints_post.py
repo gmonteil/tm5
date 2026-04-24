d1bd317a4b7f5e07a84392828a7d10d1318f197e
Michael Vossbeck <Michael.Vossbeck@Inversion-Lab.com>
2026-04-24 17:03:55 +0200

    modified: -> footprint postprocessing script relies on library  I/O routine,    added capability to write footprint (@1x1 degree) to NetCDF file.

--- postrun/footprints_post.py
+++ postrun/footprints_post.py
@@ -5,8 +5,8 @@ import sys
 import os
 from omegaconf import OmegaConf, DictConfig
 from pathlib import Path
+import datetime as dtm
 from loguru import logger
-from omegaconf import OmegaConf
 from pandas import date_range, DataFrame
 from pandas import Timestamp, Timedelta, concat
 import xarray as xr
@@ -19,6 +19,7 @@ from matplotlib.pyplot import subplots,colorbar
 from cartopy import crs
 from cartopy.mpl.gridliner import LONGITUDE_FORMATTER, LATITUDE_FORMATTER
 from tm5.fitic import read_obs_table
+from tm5.post.footprint_io import load_adjoint_fitic_footprint
 from tm5.gridtools import TM5Grids
 from tm5.post.plot_util import cnorm_set
 from tm5.post.utilities import lonstr,latstr
@@ -66,135 +67,6 @@ def load_adjemis_region_total( conf : DictConfig, obs_table : DataFrame ) -> Dat
     return fwd
 
 
-def load_adjoint_footprint(conf : DictConfig, obs_table : DataFrame) -> DataFrame:
-    outpath = Path(conf.run.paths.output)/'adjemis'
-    regions = conf.run.regions
-    start = Timestamp(conf.run.start)
-    end = Timestamp(conf.run.end)
-    # Load the observations tabled
-    tracers = set(obs_table.tracer)
-
-    footprints = { 'ilat':[],
-                   'ilon':[],
-                   'itime':[],
-                   'value':[],
-                   'region':[],
-                   'itrac':[]   }
-    coords_table = {}
-    for itrac, tracer in enumerate(tracers):
-        for iday, day in enumerate(date_range(start, end, freq='d')):
-            for ireg, region in enumerate(regions):
-                fname = outpath / day.strftime(f'adjemis.{region}.%Y%m%d.nc')
-                if not fname.exists():
-                    continue
-                with Dataset(fname, 'r') as ds:
-                    ilat = ds['ilat'][:].data
-                    ilon = ds['ilon'][:].data
-                    itrac = ds['itrac'][:].data
-                    values = ds['values'][:].data
-                    lonc = ds['lon'][:].data
-                    latc = ds['lat'][:].data
-                ndat = len(ilat)
-
-                footprints['ilat'].extend(ilat)
-                footprints['ilon'].extend(ilon)
-                footprints['itime'].extend([iday] * ndat)
-                footprints['value'].extend(values)
-                footprints['itrac'].extend(itrac)
-                footprints['region'].extend([region] * ndat)
-                #-- store spatial coordinates for region (only once)
-                if not region in coords_table:
-                    coords_table[region] = (lonc,latc)
-    footprints = DataFrame.from_dict(footprints)
-
-    # print(f"-"*30)
-    # for k,v in coords_table.items():
-    #     lonc,latc = v
-    #     dlon = np.unique(np.diff(lonc))
-    #     dlat = np.unique(np.diff(latc))
-    #     print(f"{k}, dlon/dlat = {dlon}/{dlat}:")
-    #     print(f"   lon_centers: {lonc}")
-    #     print(f"   lat_centers: {latc}")
-    # print(f"-"*30)
-    # Correct the halos:
-    r2 = footprints[footprints.region == 'eur300x200']
-    # print("r2.ilon=",r2.ilon.values)
-    # print("r2.ilat=",r2.ilat.values)
-    footprints.loc[footprints.region == 'eur300x200', 'ilon'] = r2.ilon.values - 2
-    footprints.loc[footprints.region == 'eur300x200', 'ilat'] = r2.ilat.values - 2
-    r3 = footprints[footprints.region == 'gns100x100']
-    # print("r3.ilon=",r3.ilon.values)
-    # print("r3.ilat=",r3.ilat.values)
-    footprints.loc[footprints.region == 'gns100x100', 'ilon'] = r3.ilon.values - 3
-    footprints.loc[footprints.region == 'gns100x100', 'ilat'] = r3.ilat.values - 2
-
-    # ------------------
-    # Reproject the eur3x2 data on a 1x1 grid
-    fpreg = footprints[footprints.region == 'eur300x200']
-    grid0 = TM5Grids.from_corners(west=-36+6, east=54-6, south=22+4, north=74-4, dlon=3, dlat=2)
-    grid1 = TM5Grids.from_corners(west=-36+6, east=54-6, south=22+4, north=74-4, dlon=1, dlat=1)
-
-    # Duplicate the points
-    rlon, rlat = 3, 2
-    # print(f"#ilat={len(fpreg.ilat.values)}, #ilon={len(fpreg.ilon.values)}")
-    # x = tile(fpreg.value.values, (rlat, rlon, 1))
-    # print(f"x.shape={x.shape}")
-    # x = x.reshape(-1)
-    # print(f"x.shape={x.shape}")
-    ilat = tile(fpreg.ilat.values * rlat, (rlat, rlon, 1))
-    ilon = tile(fpreg.ilon.values * rlon, (rlat, rlon, 1))
-    values = tile(fpreg.value.values, (rlat, rlon, 1)).reshape(-1)
-    itime = tile(fpreg.itime.values, (rlat, rlon, 1)).reshape(-1)
-    itrac = tile(fpreg.itrac.values, (rlat, rlon, 1)).reshape(-1)
-
-    # Adjust the indices
-    for jj in range(rlat):
-        ilat[jj, :, :] += jj
-    for ii in range(rlon):
-        ilon[:, ii, :] += ii
-
-    # Re-create a DataFrame:
-    df_eur = DataFrame.from_dict(dict(ilat=ilat.reshape(-1), ilon=ilon.reshape(-1), value=values, itime=itime, itrac=itrac))
-    df_eur.loc[:, 'region'] = 'eur300x200'
-
-    # Adjust the indices to those of a glb1x1 grid:
-    df_eur.loc[:, 'ilat'] += grid1.south + 90
-    df_eur.loc[:, 'ilon'] += grid1.west + 180
-
-    # ------------------
-    # Reproject the glb6x4 grid on glo1x1
-
-    fpreg = footprints[footprints.region == 'glb600x400']
-    grid0 = TM5Grids.from_corners(west=-180, east=180, south=-90, north=90, dlon=6, dlat=4)
-    grid1 = TM5Grids.from_corners(west=-180, east=180, south=-90, north=90, dlon=1, dlat=1)
-
-    rlon, rlat = 6, 4
-    ilat = tile(fpreg.ilat.values * rlat, (rlat, rlon, 1))
-    ilon = tile(fpreg.ilon.values * rlon, (rlat, rlon, 1))
-    values = tile(fpreg.value.values, (rlat, rlon, 1)).reshape(-1)
-    itime = tile(fpreg.itime.values, (rlat, rlon, 1)).reshape(-1)
-    itrac = tile(fpreg.itrac.values, (rlat, rlon, 1)).reshape(-1)
-
-    for jj in range(rlat):
-        ilat[jj, :, :] += jj
-    for ii in range(rlon):
-        ilon[:, ii, :] += ii
-
-    df_glb = DataFrame.from_dict(dict(ilat=ilat.reshape(-1), ilon=ilon.reshape(-1), value=values, itime=itime, itrac=itrac))
-    df_glb.loc[:, 'region'] = 'eur600x400'
-
-    # ---------------------
-    # Finally reproject gns1x1 on glo1x1
-    df_gns = footprints[footprints.region == 'gns100x100']
-    grid0 = TM5Grids.from_corners(west=0+3, east=18-3, south=42+2, north=58-2, dlon=1, dlat=1)
-    df_gns.loc[:, 'ilat'] += grid0.south + 90
-    df_gns.loc[:, 'ilon'] += grid0.west + 180
-
-    # ----------------------
-    # Concatenate all 3 dataframes
-
-    df_tot = concat([df_eur, df_glb, df_gns])
-    return df_tot
 
 
 ################################################################################
@@ -207,7 +79,7 @@ parser.add_argument('config_file',
 parser.add_argument('-m', '--host', default=os.environ['TM5_HOST'],
                     help="""selected machine/host from configuration file (default: %(default)s).""")
 parser.add_argument('--mode',
-                    choices=['visu_footp','visu_timeseries',],
+                    choices=['write_footp', 'visu_footp','visu_timeseries',],
                     default='visu_footp',
                     help="""requested analysis or visualisation (default: %(default)s).""")
 # parser.add_argument('--trange',
@@ -245,7 +117,8 @@ parser.add_argument('--extent',
                     help="""selected domain for spatial footprint plots (default: %(default)s).""")
 parser.add_argument('--outdir',
                     help="""top-level directory for any generated outputs..""")
-
+parser.add_argument('--outname',
+                    help="""explictly specifed name of output file (might be ignored in case the request yields multiple files).""")
 
 ################################################################################
 #
@@ -263,7 +136,7 @@ conf = OmegaConf.load(str(yaml_file))
 conf.host = conf[host]
 tstart = Timestamp(conf.run.start)
 tend   = Timestamp(conf.run.end)
-#-- temporal range (last day excluded)
+#-- temporal range (simulation ends at last day 0:00, this day is (typically) not simulated!)
 trange = list(date_range(conf.run.start, conf.run.end, freq='1d'))[:-1]
 nday = len(trange)
 # Load the observations tabled
@@ -299,9 +172,6 @@ for row in obs_table.iterrows():
         case other:
             msg = f"sampling strategy ***{_cols.sampling_strategy}*** not handled yet."
             logger.error(msg)
-# print(obs_table.columns)
-# print(obs_table.dtypes)
-# sys.exit(0)
 
 #
 #--
@@ -311,6 +181,76 @@ if args.mode=='visu_timeseries':
     print(fwd.head(n=3))
     msg = f"time-series plot not yet completed!"
     raise NotImplementedError(msg)
+
+elif args.mode=='write_footp':
+    #=====================================================
+    # read footprints
+    #=====================================================
+    logger.info(f"start reading footprints...")
+    fpresult = load_adjoint_fitic_footprint(conf)
+    fp = fpresult.data
+    logger.info(f"...footprints done.")
+    if not obsids==fpresult.obsids:
+        msg = f"...inconsistency between observation identifiers from obstable " \
+            f"({obsids}, {conf.observations.file}) and those from adjoint emissions " \
+            f"({fpresult.tracer})"
+        raise RuntimeError(msg)
+    adjemis_units = fp.attrs['units']
+    #-- dump some statistics
+    msg = f"...global adjoint emissions assembled, " \
+        f"min/mean/max = {fp.values.min()}/{fp.values.mean()}/{fp.values.max()} [{adjemis_units}]"
+    logger.info(msg)
+    #
+    #-- turn data array to dataset
+    #
+    ds = fp.to_dataset(name='footprint')
+    #
+    #-- add time variable
+    #
+    refday = fpresult.days[0]
+    days_since = [ (_-refday).days for _ in fpresult.days ]
+    da = xr.DataArray(
+        days_since,
+        dims=('iday',),
+        # coords = {
+        #     'iday': fp.iday
+        #     },
+        attrs = {
+            'units': refday.strftime(f"days since %Y-%m-%d")
+            }
+        )
+    ds['time'] = da
+    #
+    #-- add obsids (very likely corresponding to stations)
+    #
+    da = xr.DataArray(
+        fpresult.obsids,
+        dims=('iobs',),
+        )
+    ds['obsids'] = da
+    #
+    #-- global attributes
+    #
+    ds.attrs['tm5_rundir']  = conf.run.paths.output
+    ds.attrs['date_created'] = dtm.datetime.utcnow().isoformat()
+    #
+    #-- write to file
+    #
+    if len(obsids)>4:
+        obs_tag = f"{len(obsids)}obs"
+    else:
+        obs_tag = '--'.join( [_.replace('_','-') for _ in fpresult.obsids] )
+    time_tag = "{fpresult.days[0].strftime('%Y%m%d')}-{fpresult.days[-1].strftime('%Y%m%d')}"
+    outname_tokens = ['tm5-footprint', obs_tag, time_tag,]
+    outfile = args.outname if args.outname!=None else '_'.join(outname_tokens)+'.nc'
+    if args.outdir!=None:
+        if Path(outfile).is_absolute():
+            pass
+        else:
+            outfile = Path(args.outdir) / outfile
+    Path(outfile).parent.mkdir(parents=True, exist_ok=True)
+    ds.to_netcdf(str(outfile))
+    logger.info(f"generated file ***{outfile}***")
 
 elif args.mode=='visu_footp':
     lonw, lone, lats, latn = args.extent
@@ -328,17 +268,19 @@ elif args.mode=='visu_footp':
     # read footprints
     #=====================================================
     logger.info(f"start reading footprints...")
-    df_tot = load_adjoint_footprint(conf, obs_table)
+    # df_tot = load_adjoint_footprint(conf, obs_table)
+    fpresult = load_adjoint_fitic_footprint(conf)
+    fp = fpresult.data
     logger.info(f"...footprints done.")
-
-    # Store all footprints in a single array
-    # - array is global with dimensions time/lat/lon/obs
-    #
-    fp = zeros((nday, 180, 360, nobs))
-    fp[df_tot.itime, df_tot.ilat, df_tot.ilon, df_tot.itrac] = df_tot.value
-    adjemis_units = "ppb/kgCH4/cell/s"
+    if not obsids==fpresult.obsids:
+        msg = f"...inconsistency between observation identifiers from obstable " \
+            f"({obsids}, {conf.observations.file}) and those from adjoint emissions " \
+            f"({fpresult.tracer})"
+        raise RuntimeError(msg)
+    adjemis_units = fp.attrs['units']
+    #-- dump some statistics
     msg = f"...global adjoint emissions assembled, " \
-        f"min/mean/max = {fp.min()}/{fp.mean()}/{fp.max()} [{adjemis_units}]"
+        f"min/mean/max = {fp.values.min()}/{fp.values.mean()}/{fp.values.max()} [{adjemis_units}]"
     logger.info(msg)
     #
     #--
@@ -349,26 +291,13 @@ elif args.mode=='visu_footp':
     logger.info(msg)
     fp = fp / grid1x1.area[np.newaxis,:,:,np.newaxis]
     msg = f"...global footprint statistics after unit conversion, " \
-        f"min/mean/max = {fp.min()}/{fp.mean()}/{fp.max()} [{footp_units}]"
+        f"min/mean/max = {fp.values.min()}/{fp.values.mean()}/{fp.values.max()} [{footp_units}]"
     logger.info(msg)
-    da = xr.DataArray(
-        fp,
-        dims=('iday','lat','lon','itrac'),
-        coords = {
-            'iday': np.arange(nday),
-            'lat': grid1x1.latc,
-            'lon': grid1x1.lonc,
-            'itrac': np.arange(nobs)
-            }
-        )
-    #
-    glbmin = da.min().values
-    glbmean = da.mean().values
-    glbmax  = da.max().values
-    print(f"glbmin/glbmean/glbmax =  {glbmin:.15f}/{glbmean:.15f}/{glbmax:.15f}")
-    
-    for itrac,obsid in enumerate(obsids):# in range(len(obsids)):
-        obsid = obsids[itrac]
+    glbmin = fp.min().values
+    glbmean = fp.mean().values
+    glbmax  = fp.max().values
+    for iobs,obsid in enumerate(obsids):# in range(len(obsids)):
+        obsid = obsids[iobs]
         obs_info = obs_table.loc[obsid,'obstime_info']
         if 'station_name' in obs_table.columns:
             staid = obs_table.loc[obsid,'station_name']
@@ -383,7 +312,7 @@ elif args.mode=='visu_footp':
         #
         #-- footprint restricted to plotted domain
         #
-        datrac = da.sel(iday=np.arange(nday),lat=slice(lats,latn),lon=slice(lonw,lone),itrac=itrac)
+        datrac = fp.sel(iday=np.arange(nday),lat=slice(lats,latn),lon=slice(lonw,lone),iobs=iobs)
         dmin = datrac.min().values
         dmean = datrac.mean().values
         dmax  = datrac.max().values
@@ -392,7 +321,7 @@ elif args.mode=='visu_footp':
         #-- footprint w.r.t. to contributions from all time(s)
         #
         if args.footp_tmode=='total':
-            fpplot = fp[..., itrac].sum(0) #-- add contribution from all (daily) sensitivity
+            fpplot = fp[..., iobs].sum(0) #-- add contribution from all (daily) sensitivity
             f, ax = subplots(1, 1, figsize=args.figsize, subplot_kw=dict(projection=crs.PlateCarree()))
             ax.set_global()
             ax.coastlines()
@@ -426,18 +355,18 @@ elif args.mode=='visu_footp':
         elif args.footp_tmode=='daily':
             for iday,day in enumerate(trange):
                 #-- contribution from current day
-                fpplot = fp[iday,..., itrac] #-- global field(!)
+                fpplot = fp[iday,..., iobs] #-- global field(!)
                 #-- dataset restricted to domain for plot
-                daplot = da.sel(iday=iday,lat=slice(lats,latn),lon=slice(lonw,lone),itrac=itrac)
+                daplot = fp.sel(iday=iday,lat=slice(lats,latn),lon=slice(lonw,lone),iobs=iobs)
                 vmin = fpplot.min()
                 vmean = fpplot.mean()
                 vmax = fpplot.max()
                 pltmin  = daplot.min().values
                 pltmean = daplot.mean().values
                 pltmax  = daplot.max().values
-                # msg = f"...@{day},itrac={itrac}: vmin/vmean/vmax = {vmin}/{vmean}/{vmax}"
+                # msg = f"...@{day},iobs={iobs}: vmin/vmean/vmax = {vmin}/{vmean}/{vmax}"
                 # logger.info(msg)
-                msg = f"...@{day},itrac={itrac}: {pltmin}/{pltmean}/{pltmax}"
+                msg = f"...@{day},iobs={iobs}: {pltmin}/{pltmean}/{pltmax}"
                 logger.info(msg)
                 f, ax = subplots(1, 1, figsize=args.figsize,
                                  subplot_kw=dict(projection=crs.PlateCarree()))
